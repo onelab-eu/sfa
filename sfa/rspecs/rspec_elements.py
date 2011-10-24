040d2b6fa1220aabbfa57b1cb78d1cc21bbc8e45
Tony Mack <tmack@paris.CS.Princeton.EDU>
2011-10-24 16:56:58 -0400

    added component manager element type

--- sfa/rspecs/rspec_elements.py
+++ sfa/rspecs/rspec_elements.py
@@ -1,10 +1,17 @@
 from sfa.util.enumeration import Enum
 
 # recognized top level rspec elements
-RSpecElements = Enum('NETWORK', 'NODE', 'SLIVER', 'INTERFACE', 'LINK', 'VLINK')
+RSpecElements = Enum(NETWORK='NETWORK', 
+                     COMPONENT_MANAGER='COMPONENT_MANAGER', 
+                     SLIVER='SLIVER', 
+                     NODE='NODE', 
+                     INTERFACE='INTERFACE', 
+                     LINK='LINK', 
+                     SERVICE='SERVICE'
+                )
 
 class RSpecElement:
-    def __init__(self, element_type, name, path):
+    def __init__(self, element_type, path):
         if not element_type in RSpecElements:
             raise InvalidRSpecElement(element_type)
         self.type = element_type
