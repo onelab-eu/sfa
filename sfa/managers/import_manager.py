e17f1d189f1e44db86e50e1902d1694f565e3e8e
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-10-24 15:42:30 +0200

    import_manager a bit more verbose

--- sfa/managers/import_manager.py
+++ sfa/managers/import_manager.py
@@ -1,3 +1,5 @@
+from sfa.util.sfalogging import logger
+
 def import_manager(kind, type):
     """
     kind expected in ['registry', 'aggregate', 'slice', 'component']
@@ -6,14 +8,19 @@ def import_manager(kind, type):
     basepath = 'sfa.managers'
     qualified = "%s.%s_manager_%s"%(basepath,kind,type)
     generic = "%s.%s_manager"%(basepath,kind)
+
+    message="import_manager for kind=%s and type=%s"%(kind,type)
     try: 
         manager = __import__(qualified, fromlist=[basepath])
+        logger.info ("%s: loaded %s"%(message,qualified))
     except:
         try:
             manager = __import__ (generic, fromlist=[basepath])
             if type != 'pl' : 
-                logger.warn ("Using generic manager for %s with type=%s"%(kind,type))
+                logger.warn ("%s: using generic with type!='pl'"%(message))
+            logger.info("%s: loaded %s"%(message,generic))
         except:
             manager=None
+            logger.log_exc("%s: unable to import either %s or %s"%(message,qualified,generic))
     return manager
     
