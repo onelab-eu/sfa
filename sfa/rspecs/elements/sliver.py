3f5f00f6f9906ba6d3bbcf72971a727c2380967c
Tony Mack <tmack@paris.CS.Princeton.EDU>
2011-11-07 13:57:18 -0500

    added client_id field

--- sfa/rspecs/elements/sliver.py
+++ sfa/rspecs/elements/sliver.py
@@ -2,8 +2,8 @@ from sfa.rspecs.elements.element import Element
 
 class Sliver(Element):
     fields = {
+        'client_id': None,
         'name': None,
         'tags': [],
         'slice_id': None,
-        
     }
