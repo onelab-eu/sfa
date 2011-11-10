4ebcc1b175ddfa3c4c0173d70b304e0c9a4facc1
Tony Mack <tmack@paris.CS.Princeton.EDU>
2011-11-09 21:27:35 -0500

    added generic get() and add() methods

--- sfa/rspecs/elements/element.py
+++ sfa/rspecs/elements/element.py
@@ -7,3 +7,17 @@ class Element(dict):
         dict.__init__(self, self.fields) 
         self.update(fields)
 
+    @staticmethod
+    def get(element_class, xml, xpath, namespaces=None):
+        elems = xml.xpath(xpath, namespaces)
+        generic_elems = [element_class(elem.attrib, elem) for elem in elems]
+        return generic_elems
+
+
+    @staticmethod
+    def add(xml, element_class, name, obj):
+        elem = xml.add_element(name)
+        for field in element_class.fields:
+            if field in obj and obj[field]:
+                elem.set(field, obj[field])
+        return elem
