306fa2429406038b451ba4b8bf716ea9112b2238
Tony Mack <tmack@paris.CS.Princeton.EDU>
2011-11-10 07:34:32 -0500

    added component_id

--- sfa/rspecs/elements/sliver.py
+++ sfa/rspecs/elements/sliver.py
@@ -3,6 +3,7 @@ from sfa.rspecs.elements.element import Element
 class Sliver(Element):
     fields = {
         'sliver_id': None,
+        'component_id': None,
         'client_id': None,
         'name': None,
         'tags': [],
