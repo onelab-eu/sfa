926a4dbaccf7c6edddf239733eba3eb30b7c2c2a
Tony Mack <tmack@paris.CS.Princeton.EDU>
2011-11-10 21:27:28 -0500

    renamed 'name' to 'tagname'

--- sfa/rspecs/elements/versions/sfav1PLTag.py
+++ sfa/rspecs/elements/versions/sfav1PLTag.py
@@ -5,7 +5,7 @@ class SFAv1PLTag:
     @staticmethod
     def add_pl_tags(xml, pl_tags):
         for pl_tag in pl_tags:
-            pl_tag_elem = xml.add_element(pl_tag['name'])
+            pl_tag_elem = xml.add_element(pl_tag['tagname'])
             pl_tag_elem.set_text(pl_tag['value'])
               
     @staticmethod
@@ -13,7 +13,7 @@ class SFAv1PLTag:
         pl_tags = []
         for elem in xml.iterchildren():
             if elem.tag not in ignore:
-                pl_tag = PLTag({'name': elem.tag, 'value': elem.text})
+                pl_tag = PLTag({'tagname': elem.tag, 'value': elem.text})
                 pl_tags.appen(pl_tag)    
         return pl_tags
 
