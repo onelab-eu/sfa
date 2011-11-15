7b43289bae04b50b162abb03526793585a116d12
Tony Mack <tmack@paris.CS.Princeton.EDU>
2011-11-14 22:34:39 -0500

    added add_sliver_attributes method

--- sfa/rspecs/elements/versions/sfav1Sliver.py
+++ sfa/rspecs/elements/versions/sfav1Sliver.py
@@ -13,12 +13,16 @@ class SFAv1Sliver:
             slivers = [slivers]
         for sliver in slivers:
             sliver_elem = Element.add_elements(xml, 'sliver', sliver, ['name'])[0]
+            SFAv1Sliver.add_sliver_attributes(sliver_elem, sliver.get('tags', []))
             if sliver.get('sliver_id'):
                 sliver_id_leaf = Xrn(sliver.get('sliver_id')).get_leaf()
                 sliver_id_parts = sliver_id_leaf.split(':')
                 name = sliver_id_parts[0]
                 sliver_elem.set('name', name)
-            SFAv1PLTag.add_pl_tags(sliver_elem, sliver.get('tags', []))
+
+    @staticmethod
+    def add_sliver_attributes(xml, attributes):
+        SFAv1PLTag.add_pl_tags(xml, attributes)
                     
     @staticmethod
     def get_slivers(xml, filter={}):
@@ -29,6 +33,10 @@ class SFAv1Sliver:
             sliver = Sliver(sliver_elem.attrib,sliver_elem)
             if 'component_id' in xml.attrib:     
                 sliver['component_id'] = xml.attrib['component_id']
-            sliver['tags'] = SFAv1PLTag.get_pl_tags(sliver_elem, ignore=Sliver.fields.keys())
+            sliver['tags'] = SFAv1Sliver.get_sliver_attributes(sliver_elem)
             slivers.append(sliver)
-        return slivers            
+        return slivers           
+
+    @staticmethod
+    def get_sliver_attributes(xml, filter={}):
+        return SFAv1PLTag.get_pl_tags(xml, ignore=Sliver.fields.keys())     
