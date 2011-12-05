47a65cf5e399ce943d62a2b9db62c5d5ae1645ac
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-12-05 18:43:07 +0100

    example of an alternative flavour

---
+++ sfa/generic/max.py
@@ -0,0 +1,21 @@
+# an example of how to plugin the max aggregate manager with the flavour model
+# might need to be tested
+# 
+from sfa.generic.pl import pl
+
+import sfa.managers.aggregate_manager_max
+
+class max (pl):
+
+# the max flavour behaves like pl, except for 
+# the aggregate
+    def aggregate_manager_class (self) :
+        return sfa.managers.aggregate_manager_max.AggregateManagerMax
+
+# I believe the component stuff is not implemented
+    def component_manager_class (self):
+        return None
+    def component_driver_class (self):
+        return None
+
+
