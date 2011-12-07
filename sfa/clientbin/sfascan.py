371abd1603ae28b9f06b1be4dd748f9ed0e04ad6
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-12-07 11:32:10 +0100

    adding missing files

---
+++ sfa/clientbin/sfascan.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from sfa.client.sfascan import SfaScan
+
+if __name__ == '__main__':
+    SfaScan().main()
---
+++ sfa/clientbin/sfi.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# sfi -- slice-based facility interface
+
+from sfa.client.sfi import Sfi
+
+if __name__ == "__main__":
+    Sfi().main()
