40c9845212ec4b5fa0c8b54e718171d529826845
smbaker <smbaker@fc8clean.lan>
2011-12-29 19:38:34 -0800

    fix protogeni rspec converter

--- sfa/rspecs/pg_rspec_converter.py
+++ sfa/rspecs/pg_rspec_converter.py
@@ -51,21 +51,22 @@ class PGRSpecConverter:
 
         # get network
         networks = pg_rspec.version.get_networks()
-        network_hrn = networks[0]
+        network_hrn = networks[0]["name"]
         network_element = sfa_rspec.xml.add_element('network', name=network_hrn, id=network_hrn)
         
         # get nodes
-        pg_nodes_elements = pg_rspec.version.get_node_elements()
+        pg_nodes_elements = pg_rspec.version.get_nodes()
         nodes_with_slivers = pg_rspec.version.get_nodes_with_slivers()
         i = 1
-        for pg_node_element in pg_nodes_elements:
-            attribs = dict(pg_node_element.attrib.items()) 
+        for pg_node in pg_nodes_elements:
+            attribs = dict(pg_node.items())
             attribs['id'] = 'n'+str(i)
             
             node_element = network_element.add_element('node')
             for attrib in attribs:
-                node_element.set(attrib, attribs[attrib])
-            urn = pg_node_element.xpath('@component_id', namespaces=pg_rspec.namespaces)
+                if type(attribs[attrib]) == str:
+                    node_element.set(attrib, attribs[attrib])
+            urn = pg_node["component_id"]
             if urn:
                 urn = urn[0]
                 hostname = Xrn.urn_split(urn)[-1]
@@ -75,8 +76,8 @@ class PGRSpecConverter:
                     node_element.add_element('sliver')  
                      
             # just copy over remaining child elements  
-            for child in pg_node_element.getchildren():
-                node_element.append(transform(child).getroot())
+            #for child in pg_node_element.getchildren():
+            #    node_element.append(transform(child).getroot())
             i = i+1
  
         return sfa_rspec.toxml()
