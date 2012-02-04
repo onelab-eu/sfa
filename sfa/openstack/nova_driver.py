b9efb871eb597b0efa0ab468f221ce2a8d7c60a5
Tony Mack <tmack@paris.CS.Princeton.EDU>
2012-02-03 22:29:51 -0500

    Fix get resources for slices

--- sfa/openstack/nova_driver.py
+++ sfa/openstack/nova_driver.py
@@ -128,7 +128,7 @@ class NovaDriver (Driver):
             elif record['type'] == 'slice': 
                 os_record = self.shell.auth_manager.get_project(name)
                 record['description'] = os_record.description
-                record['PI'] = self.hrn + "." + os_record.project_manager.name
+                record['PI'] = [self.hrn + "." + os_record.project_manager.name]
                 record['geni_creator'] = record['PI'] 
                 record['researcher'] = [self.hrn + "." + user for \
                                          user in os_record.member_ids]
