9ccab1d9285ffba1fe79a6c27efe56c823e945f6
Tony Mack <tmack@paris.CS.Princeton.EDU>
2012-03-14 16:36:36 -0400

    initial checkin

---
+++ sfa/rspecs/elements/versions/plosv1FWRule.py
@@ -0,0 +1,23 @@
+from sfa.rspecs.elements.element import Element  
+from sfa.rspecs.elements.fw_rule import FWRule
+
+class PLOSv1FWRule:
+    @staticmethod
+    def add_rules(xml, rules):
+        if not rules:
+            return 
+        for rule in rules:
+            rule_elem = xml.add_element('plos:fw_rule')
+            rule_elem.set('protocol', rule.get('protocol'))
+            rule_elem.set('port_range', rule.get('port_range'))
+            rule_elem.set('cidr_ip', rule.get('cidr_ip'))
+            rule_elem.set('icmp_type_code', rule.get('icmp_type_code'))
+              
+    @staticmethod
+    def get_rules(xml):
+        rules = []
+        for rule_elem in xml.xpath('./plos:fw_rule | ./fw_rule'):
+            rule = FWRule(rule_elem.attrib, rule_elem)
+            rules.append(rule)  
+        return rules
+
