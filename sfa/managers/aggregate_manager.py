1c96705f2558015856c27ffb5dcc4d3b4f9c93fc
Tony Mack <tmack@paris.CS.Princeton.EDU>
2012-04-12 20:47:11 -0400

    fix urn

--- sfa/managers/aggregate_manager.py
+++ sfa/managers/aggregate_manager.py
@@ -49,7 +49,7 @@ class AggregateManager:
         call_id = options.get('call_id')
         if Callids().already_handled(call_id): return {}
     
-        xrn = Xrn(xrn)
+        xrn = Xrn(xrn,'slice')
         slice_urn=xrn.get_urn()
         slice_hrn=xrn.get_hrn()
         return self.driver.sliver_status (slice_urn, slice_hrn)
