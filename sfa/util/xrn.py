b99599579ebf6f4a4d0eca53bcc738b333227f06
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2012-06-01 14:51:19 +0200

    somehow I just went across a urn that had been lower()'ed tweaked xrn so that gets handled gracefully

--- sfa/util/xrn.py
+++ sfa/util/xrn.py
@@ -92,16 +92,20 @@ class Xrn:
                 return True
         return False
 
-    URN_PREFIX = "urn:publicid:IDN"
-
     ########## basic tools on URNs
+    URN_PREFIX = "urn:publicid:idn"
+
+    @staticmethod
+    def is_urn (text):
+        return text.lower().startswith(Xrn.URN_PREFIX)
+
     @staticmethod
     def urn_full (urn):
-        if urn.startswith(Xrn.URN_PREFIX): return urn
+        if Xrn.is_urn(urn): return urn
         else: return Xrn.URN_PREFIX+urn
     @staticmethod
     def urn_meaningful (urn):
-        if urn.startswith(Xrn.URN_PREFIX): return urn[len(Xrn.URN_PREFIX):]
+        if Xrn.is_urn(urn): return urn[len(Xrn.URN_PREFIX):]
         else: return urn
     @staticmethod
     def urn_split (urn):
@@ -117,7 +121,7 @@ class Xrn:
     def __init__ (self, xrn, type=None):
         if not xrn: xrn = ""
         # user has specified xrn : guess if urn or hrn
-        if xrn.startswith(Xrn.URN_PREFIX):
+        if Xrn.is_urn(xrn):
             self.hrn=None
             self.urn=xrn
             self.urn_to_hrn()
@@ -180,7 +184,7 @@ class Xrn:
         """
         
 #        if not self.urn or not self.urn.startswith(Xrn.URN_PREFIX):
-        if not self.urn.startswith(Xrn.URN_PREFIX):
+        if not Xrn.is_urn(self.urn):
             raise SfaAPIError, "Xrn.urn_to_hrn"
 
         parts = Xrn.urn_split(self.urn)
@@ -214,7 +218,7 @@ class Xrn:
         """
 
 #        if not self.hrn or self.hrn.startswith(Xrn.URN_PREFIX):
-        if self.hrn.startswith(Xrn.URN_PREFIX):
+        if Xrn.is_urn(self.hrn):
             raise SfaAPIError, "Xrn.hrn_to_urn, hrn=%s"%self.hrn
 
         if self.type and self.type.startswith('authority'):
