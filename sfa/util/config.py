177b96762d2b0d94af773c14e5a7cee161be3129
Tony Mack <tmack@paris.CS.Princeton.EDU>
2012-08-05 20:09:28 -0400

    fix out output_shell handles bools

--- sfa/util/config.py
+++ sfa/util/config.py
@@ -162,8 +162,8 @@ DO NOT EDIT. This file was automatically generated at
                 # bash does not have the concept of NULL
                 if value:
                     option = "%s_%s" % (section.upper(), name.upper())
-                    if not value.isdigit() and not bool(value):
-                        value = "'%s'" % value  
+                    if bool(value) and not value.isdigit():
+                        value = str(bool(value))  
                     buf.write(option + "=" + value + os.linesep)
         return buf.getvalue()        
 
