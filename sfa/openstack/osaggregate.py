11881f631403e199ecbb86f181610810ba51f534
Tony Mack <tmack@paris.CS.Princeton.EDU>
2012-08-29 18:01:49 -0400

    preserve node client id from request rspec

--- sfa/openstack/osaggregate.py
+++ sfa/openstack/osaggregate.py
@@ -109,6 +109,9 @@ class OSAggregate:
                 node_dict[node_xrn.urn] = rspec_node
             else:
                 rspec_node = node_dict[node_xrn.urn]
+
+            if instance.metadata.get('client_id'):
+                rspec_node['client_id'] = instance.metadata.get('client_id')
             
             flavor = self.driver.shell.nova_manager.flavors.find(id=instance.flavor['id'])
             sliver = instance_to_sliver(flavor)
@@ -280,6 +283,8 @@ class OSAggregate:
                     metadata['security_groups'] = group_name
                     if node.get('component_id'):
                         metadata['component_id'] = node['component_id']
+                    if node.get('client_id'):
+                        metadata['client_id'] = node['client_id']
                     self.driver.shell.nova_manager.servers.create(flavor=flavor_id,
                                                             image=image_id,
                                                             key_name = key_name,
