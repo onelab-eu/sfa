bffcae00603ea525170eba13d197fd1f0bcebe44
Sandrine Avakian <sandrine.avakian@inria.fr>
2012-10-11 09:42:17 +0200

    .

--- sfa/senslab/config/ldap_config.py
+++ sfa/senslab/config/ldap_config.py
@@ -1,6 +1,7 @@
 #For Admin rights:
+LDAP_IP_ADDRESS = '194.199.16.166'
 LDAP_WEB_DN = 'uid=web,ou=Service,dc=senslab,dc=info' 
-LDAP_WEB_PASSWORD = 
+
 
 LDAP_PEOPLE_DN = 'ou=People,dc=senslab,dc=info'
 
@@ -10,4 +11,4 @@ LDAP_USER_UID_NUMBER_MIN = '2000'
 LDAP_USER_GID_NUMBER = '2000' 
 LDAP_USER_HOME_PATH = '/senslab/users/' 
 
-LDAP_IP_ADDRESS = '194.199.16.166'
+
