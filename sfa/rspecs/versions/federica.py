c07fc4fcb69543ef67e63a767d7ee48754e2d379
Tony Mack <tmack@paris.CS.Princeton.EDU>
2012-12-18 10:46:35 -0500

    fix content_type in fedrica rspec versions. Remove redundant rspec versions

--- sfa/rspecs/versions/federica.py
+++ sfa/rspecs/versions/federica.py
@@ -1,17 +1,23 @@
-from sfa.rspecs.versions.pgv2 import PGv2Ad, PGv2Request, PGv2Manifest
+from sfa.rspecs.versions.pgv2 import PGv2
 
-class FedericaAd (PGv2Ad):
+class FedericaAd (PGv2):
     enabled = True
+    type = 'Fedrica'
+    content_type = 'ad'
     schema = 'http://sorch.netmode.ntua.gr/ws/RSpec/ad.xsd'
     namespace = 'http://sorch.netmode.ntua.gr/ws/RSpec'
 
-class FedericaRequest (PGv2Request):
+class FedericaRequest (PGv2):
     enabled = True
+    type = 'Fedrica'
+    content_type = 'request'
     schema = 'http://sorch.netmode.ntua.gr/ws/RSpec/request.xsd'
     namespace = 'http://sorch.netmode.ntua.gr/ws/RSpec'
 
-class FedericaManifest (PGv2Manifest):
+class FedericaManifest (PGv2):
     enabled = True
+    type = 'Fedrica'
+    content_type = 'manifest'
     schema = 'http://sorch.netmode.ntua.gr/ws/RSpec/manifest.xsd'
     namespace = 'http://sorch.netmode.ntua.gr/ws/RSpec'
 
