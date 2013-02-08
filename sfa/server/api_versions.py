7f0832796aa604729648728328ab9a433653bf02
Tony Mack <tmack@paris.CS.Princeton.EDU>
2013-02-07 23:08:46 -0500

    better support for reporting available api versions

---
+++ sfa/server/api_versions.py
@@ -0,0 +1,47 @@
+import os
+from sfa.util.xml import XML
+from sfa.util.config import Config
+
+class ApiVersions:
+
+    required_fields = ['version', 'url']
+    
+    template = """<api_versions>
+<api_version name="" version="" url="" />
+</api_versions>""" 
+
+    def __init__(self, string=None, filename=None, create=False):
+        self.xml = None
+
+        if create:
+            self.create()
+        elif string:
+            self.load(string)
+        elif filename:
+            self.load(filename)
+        else:
+            # load the default file
+            c = Config()
+            api_versions_file = os.path.sep.join([c.config_path, 'api_versions.xml'])
+            self.load(api_versions_file)
+        
+    def create(self):
+        self.xml = XML(string=ApiVersions.template)
+
+    def load(self, source):
+        self.xml = XML(source)
+
+    def get_versions(self):
+        versions = []
+        for value in self.xml.todict().values():
+            if not value:
+                continue
+            if isinstance(value, list):
+                for item in value:
+                    if isinstance(item, dict) and \
+                       set(ApiVersions.required_fields).issubset(item.keys()):
+                        api_version = {str(item['version']): item['url']}
+                        versions.append(api_version)
+        return versions  
+                
+           
