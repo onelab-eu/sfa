8d44e1b2d606aabe455fbda136ba375e4c1ef8fd
Mohamed Larabi <mohamed.larabi@inria.fr>
2013-11-25 17:53:23 +0100

    DB Migration: fix 003_sliver_allocations.py

--- sfa/storage/migrations/versions/003_sliver_allocations.py
+++ sfa/storage/migrations/versions/003_sliver_allocations.py
@@ -1,13 +1,20 @@
-from sqlalchemy import Table, MetaData, Column, ForeignKey
+from sqlalchemy import Table, MetaData, Column
 from sqlalchemy import Integer, String
-from sfa.storage.model import SliverAllocation
 
 metadata=MetaData()
+sliver_allocation_table = \
+    Table ( 'sliver_allocation', metadata,
+            Column('sliver_id', String, primary_key=True),
+            Column('client_id', String),
+            Column('component_id', String),
+            Column('slice_urn', String),
+            Column('allocation_state', String),
+          )
 
 def upgrade(migrate_engine):
     metadata.bind = migrate_engine
-    SliverAllocation.create()
+    sliver_allocation_table.create()
 
 def downgrade(migrate_engine):
     metadata.bind = migrate_engine
-    SliverAllocation.drop()
+    sliver_allocation_table.drop()
