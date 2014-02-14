713aa5757727d4eaac05705d72d2c5c439a72d48
Jordan Augé <jordan.auge@lip6.fr>
2014-02-14 17:27:33 +0100

    Ofelia OpenFlow RSpec parsing support

---
+++ sfa/rspecs/elements/versions/ofeliav1Port.py
@@ -0,0 +1,53 @@
+from sfa.util.xrn import Xrn
+from sfa.util.xml import XmlElement
+
+from sfa.rspecs.elements.element    import Element
+from sfa.rspecs.elements.port       import Port
+
+class Ofeliav1Port:
+
+    @staticmethod
+    def add_portrs(xml, ports):
+        raise Exception, "not implemented yet"
+        if not ports:
+            return
+        if not isinstance(ports, list):
+            ports = [ports]
+        for port in ports:
+            port_elem = xml.add_instance('port', port, ['name'])
+            tags = port.get('tags', [])
+            if tags:
+                for tag in tags:
+                    Ofeliav1Port.add_port_attribute(port_elem, tag['tagname'], tag['value'])
+
+    @staticmethod
+    def add_port_attribute(xml, name, value):
+        raise Exception, "not implemented yet"
+        elem = xml.add_element(name)
+        elem.set_text(value)
+    
+    @staticmethod
+    def get_port_attributes(xml):
+        attribs = []
+        for elem in xml.iterchildren():
+            if elem.tag not in Port.fields:
+                xml_element = XmlElement(elem, xml.namespaces)
+                instance = Element(fields=xml_element, element=elem)
+                instance['name'] = elem.tag
+                instance['value'] = elem.text
+                attribs.append(instance)
+        return attribs 
+                
+    @staticmethod
+    def get_ports(xml, filter={}):
+        xpath = './openflow:port | ./port'
+        port_elems = xml.xpath(xpath)
+        ports = []
+        for port_elem in port_elems:
+            port = Port(port_elem.attrib,port_elem)
+            #if 'component_id' in xml.attrib:     
+            #    port['component_id'] = xml.attrib['component_id']
+            #port['tags'] = Ofeliav1Port.get_port_attributes(port_elem)
+            ports.append(port)
+        return ports           
+
---
+++ sfa/rspecs/elements/versions/ofeliav1link.py
@@ -0,0 +1,22 @@
+from sfa.util.sfalogging import logger
+from sfa.util.xml import XpathFilter
+from sfa.util.xrn import Xrn, get_leaf
+
+from sfa.rspecs.elements.element import Element
+from sfa.rspecs.elements.link import Link
+
+class Ofeliav1Link:
+
+    @staticmethod
+    def get_links(xml, filter={}):
+        xpath = '//link%s | //openflow:link%s' % (XpathFilter.xpath(filter), XpathFilter.xpath(filter))
+        link_elems = xml.xpath(xpath)
+        return Ofeliav1Link.get_link_objs(link_elems)
+
+    @staticmethod
+    def get_link_objs(link_elems):
+        links = []    
+        for link_elem in link_elems:
+            link = Link(link_elem.attrib, link_elem)
+            links.append(link)
+        return links
